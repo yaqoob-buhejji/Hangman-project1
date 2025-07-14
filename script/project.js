8a46361e6da2412fc5f2ed8c9713743b1470beab
Yaqoob <yaqoob.buhejji@gmail.com>
2025-07-14 15:55:40 +0300

    added the words with the points

--- script/project.js
+++ script/project.js
@@ -2,7 +2,8 @@ function init(){
 
 const output = document.getElementById('output')
 const allkey = document.querySelectorAll('.key')
-const lines = document.querySelectorAll('.list-p')
+const displyWord = document.querySelector('.p')
+let lives = 5
 
 
 // const emptyLines = ['1','2','3','4','5',];
@@ -10,19 +11,43 @@ let letter;
 let word;
 let currentLetter = [];
 
-const list = ['orange', 'apple','car','javascript','HTML'];
+const list = ['orange', 'apple','car','javascript','html'];
 
+
+function getWord(event) {
+    const randomword = list[Math.floor(Math.random()*list.length)]
+    word = randomword;
+    displyWord.innerHTML=word.split('').map(()=>`<li class="letter"></li>`).join('');
+}
+
+function playing(){
+    console.log("word " ,word)
+    console.log("letter " ,letter)
+
+    if(word.includes(letter)){
+
+        console.log(`${letter} exist`);
+
+    }
+    else{
+        lives -=1;
+        console.log("You lost 1 life: ", lives)
+    }
+}
 
 allkey.forEach((key) => {
-        key.addEventListener('click', getWord)
+        key.addEventListener('click', function(event){
+            letter = event.target.innerText.toLowerCase();
+            console.log(letter)
+            playing();
+        })
     }
 )
 
 
-function getWord(event) {
-    const word = list[Math.floor(Math.random()*list.length)]
-    console.log(word);
-}
+
+getWord();
+playing();
 }
 
 document.addEventListener('DOMContentLoaded', init)
