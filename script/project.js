aadf3faa88bb5f975c982df7c6fc3e3dfac6e399
Yaqoob <yaqoob.buhejji@gmail.com>
2025-07-14 14:59:21 +0300

    getting random word

--- script/project.js
+++ script/project.js
@@ -1,11 +1,29 @@
+function init(){
+
 const output = document.getElementById('output')
 const allkey = document.querySelectorAll('.key')
+const lines = document.querySelectorAll('.list-p')
 
-allkey.forEach(
-    key =>{
-        key.addEventListener('click',() =>{
-            output.value += key.textContent
-        })
+
+// const emptyLines = ['1','2','3','4','5',];
+let letter;
+let word;
+let currentLetter = [];
+
+const list = ['orange', 'apple','car','javascript','HTML'];
+
+
+allkey.forEach((key) => {
+        key.addEventListener('click', getWord)
     }
 )
+
+
+function getWord(event) {
+    const word = list[Math.floor(Math.random()*list.length)]
+    console.log(word);
+}
+}
+
+document.addEventListener('DOMContentLoaded', init)
 
