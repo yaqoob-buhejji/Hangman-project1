9ecfb000503e20d3303151d8c5f49b609e3841f8
Yaqoob <yaqoob.buhejji@gmail.com>
2025-07-15 10:37:01 +0300

    The pictures changes with the letters

--- script/project.js
+++ script/project.js
@@ -3,13 +3,16 @@ function init(){
 const output = document.getElementById('output')
 const allkey = document.querySelectorAll('.key')
 const displyWord = document.querySelector('.p')
-let lives = 5
+const hangImg = document.querySelector('.game-board img')
+const loseMessage = document.querySelector("#lose")
 
 
 // const emptyLines = ['1','2','3','4','5',];
 let letter;
 let word;
 let currentLetters = [];
+let lives = 0;
+let maxLives = 7;
 
 const list = ['orange', 'apple','car','javascript','html'];
 
@@ -24,6 +27,9 @@ function playing(){
     // console.log("word " ,word)
     // console.log("letter " ,letter)
 
+    if(currentLetters.length === word.length){
+        console.log("You win")
+    }
     if(word.includes(letter)){
         console.log(`${letter} exist`);
         [...word].forEach((char, i) => {
@@ -35,8 +41,32 @@ function playing(){
 
     }
     else{
-        lives -=1;
-        console.log("You lost 1 life: ", lives)
+        lives++;
+        console.log(lives)
+        console.log(loseMessage)
+        if(lives < 8){
+        hangImg.src = `./imgs/${lives+1}.png`
+
+        }
+        // if(lives === 1){
+        // console.log("In if")
+
+        //     hangImg.src = `./imgs/2nd.png`
+        // }
+
+        // losing
+        if(lives === maxLives){
+            console.log("You lose")
+            
+            loseMessage.style.visibility = "visible"
+
+            allkey.forEach((key)=>{
+                key.addEventListener("click",()=>{})
+                key.removeEventListener("click",()=>{})
+            })
+
+        }
+
     }
 }
 
@@ -52,7 +82,7 @@ allkey.forEach((key) => {
 
 
 getWord();
-playing();
+// playing();
 }
 
 document.addEventListener('DOMContentLoaded', init)
